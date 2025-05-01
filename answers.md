1013ed194e51382579ed47a39ee4bc0017a0ce8d
Zoe Murphy <zmurphy@tulane.edu>
2025-04-30 23:51:10 -0500

    commit

--- answers.md
+++ answers.md
@@ -2,7 +2,7 @@
 
 ## Answers
 
-**Name:**_________________________
+**Name:**___Zoe Murphy___
 **Name:**_________________________
 
 
@@ -11,8 +11,11 @@ Place all written answers from `recitation-08.md` here for easier grading.
 
 
 - **1b)**
+The work of the function would be: O(E) insertions and O(log V) with the push and pop operations having up to V elements so it's O(E log V)
 
+The span of the function is: O(V * logV)
 
 
 - **2b)**
+This was a coding question, my answer is in main.py
 
--- main.py
+++ main.py
@@ -13,7 +13,25 @@ def shortest_shortest_path(graph, source):
       (shortest path weight, shortest path number of edges). See test case for example.
     """
     ### TODO
-    pass
+    heap = [(0, 0, source)]  # initialize the heap
+    result = {} # store shortest path weight and edge count
+
+    while heap: # while loop
+        weight, edges, node = heappop(heap) # pop the node with smallest weight and lowest # of edges
+
+        # checking if we've looked at this node with better or equal path
+        if node in result and (weight > result[node][0] or (weight == result[node][0] and edges >= result[node][1])):
+            continue
+
+        # give this node the current shortest path
+        result[node] = (weight, edges)
+
+        # add everything updated to the heap
+        for neighbor, edge_weight in graph.get(node, []):
+            heappush(heap, (weight + edge_weight, edges + 1, neighbor))
+
+    return result
+    
     
 
     
@@ -25,7 +43,18 @@ def bfs_path(graph, source):
       that vertex in the shortest path tree.
     """
     ###TODO
-    pass
+    queue = deque([source]) # initialize queue with source code
+    parent = {source: None}  # parent of source is none cause there is no parent
+    
+    while queue: # while going through the queue
+        node = queue.popleft() # pop the first node in the queue
+        
+        for neighbor in graph.get(node, []): # look at the next nodes
+            if neighbor not in parent:  # checking if the node hasn't been parsed through
+                parent[neighbor] = node  # set the parent as this node
+                queue.append(neighbor)  # add node to the queue
+    
+    return parent
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -44,5 +73,13 @@ def get_path(parents, destination):
       (excluding the destination node itself). See test_get_path for an example.
     """
     ###TODO
-    pass
+    path = [] # building path
+    node = parents[destination]  # starting at the parent
 
+    # go backwards through tree
+    while node is not None:
+        path.append(node) # add node to path
+        node = parents[node] # move to next node
+
+    # flip list and return it 
+    return ''.join(reversed(path))
